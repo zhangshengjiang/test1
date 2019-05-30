479cd4996059dca5c36b8665fcb250b8aab50f33
Administrator <Administrator@USER-20161013UJ>
2019-05-30 15:03:16 +0800

    mao

--- test2/src/test2/dog.java
+++ test2/src/test2/dog.java
@@ -2,7 +2,7 @@ package test2;
 
 public class dog {
 public static void main(String[] args) {
-	System.out.println("wangwang!");
+	System.out.println("wangwang!-");
 }
 
 }
---
+++ test3/src/test3/cat.java
@@ -0,0 +1,7 @@
+package test3;
+
+public class cat {
+public static void main(String[] args) {
+	System.out.println("miaomiaomiao!!!");
+}
+}
